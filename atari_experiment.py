efe3d2cd94d4e37031618123b900fc363c9a70c8
steffenvan <steffem@vanfamily.com>
2019-04-14 21:11:16 +0200

    Changed logdir

--- atari_experiment.py
+++ atari_experiment.py
@@ -35,7 +35,9 @@ nest = tf.contrib.framework.nest
 flags = tf.app.flags
 FLAGS = tf.app.flags.FLAGS
 
-flags.DEFINE_string('logdir', '/tmp/agent', 'TensorFlow log directory.')
+directory = os.path.join("/tmp/agent", "Pong-v0")
+
+flags.DEFINE_string('logdir', directory, 'TensorFlow log directory.')
 flags.DEFINE_enum('mode', 'train', ['train', 'test'], 'Training or test mode.')
 
 # Flags used for testing.
