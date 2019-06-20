a174df9fe9b37b981b23cf1f431cf397707e119a
steffenvan <steffem@vanfamily.com>
2019-06-20 12:27:27 +0200

    Added resnet lstm for atari

--- agent.py
+++ agent.py
@@ -128,12 +128,107 @@ class ImpalaFeedForward(snt.AbstractModule):
 
     return snt.BatchApply(self._head)(torso_outputs), core_state
 
-class ImpalaLSTM(snt.RNNCore):
+class AtariResNetLSTM(snt.RNNCore):
+  """Agent with ResNet."""
+
+  def __init__(self, num_actions):
+    super(AtariResNetLSTMAgent, self).__init__(name='resnet_lstm_agent')
+
+    self._num_actions = num_actions
+
+    with self._enter_variable_scope():
+      self._core = tf.contrib.rnn.LSTMBlockCell(256)
+
+  def initial_state(self, batch_size):
+    return self._core.zero_state(batch_size, tf.float32)
+
+  def _torso(self, input_):
+    last_action, env_output = input_
+    reward, _, _, frame = env_output
+
+    # Convert to floats.
+    frame = tf.to_float(frame)
+    frame /= 255
+
+    with tf.variable_scope('convnet'):
+      conv_out = frame
+      for i, (num_ch, num_blocks) in enumerate([(16, 2), (32, 2), (32, 2)]):
+        # Downscale.
+        conv_out = snt.Conv2D(num_ch, 3, stride=1, padding='SAME')(conv_out)
+        conv_out = tf.nn.pool(
+            conv_out,
+            window_shape=[3, 3],
+            pooling_type='MAX',
+            padding='SAME',
+            strides=[2, 2])
+
+        # Residual block(s).
+        for j in range(num_blocks):
+          with tf.variable_scope('residual_%d_%d' % (i, j)):
+            block_input = conv_out
+            conv_out = tf.nn.relu(conv_out)
+            conv_out = snt.Conv2D(num_ch, 3, stride=1, padding='SAME')(conv_out)
+            conv_out = tf.nn.relu(conv_out)
+            conv_out = snt.Conv2D(num_ch, 3, stride=1, padding='SAME')(conv_out)
+            conv_out += block_input
+
+    conv_out = tf.nn.relu(conv_out)
+    conv_out = snt.BatchFlatten()(conv_out)
+
+    conv_out = snt.Linear(256)(conv_out)
+    conv_out = tf.nn.relu(conv_out)
+
+    # Append clipped last reward and one hot last action.
+    clipped_reward = tf.expand_dims(tf.clip_by_value(reward, -1, 1), -1)
+    one_hot_last_action = tf.one_hot(last_action, self._num_actions)
+    return tf.concat([conv_out, clipped_reward, one_hot_last_action],
+                     axis=1)
+
+  def _head(self, core_output):
+    policy_logits = snt.Linear(self._num_actions, name='policy_logits')(
+        core_output)
+    baseline = tf.squeeze(snt.Linear(1, name='baseline')(core_output), axis=-1)
+
+    # Sample an action from the policy.
+    new_action = tf.multinomial(policy_logits, num_samples=1,
+                                output_dtype=tf.int32)
+    new_action = tf.squeeze(new_action, 1, name='new_action')
+
+    return ImpalaAgentOutput(new_action, policy_logits, baseline)
+
+  def _build(self, input_, core_state):
+    action, env_output = input_
+    actions, env_outputs = nest.map_structure(lambda t: tf.expand_dims(t, 0),
+                                              (action, env_output))
+    outputs, core_state = self.unroll(actions, env_outputs, core_state)
+    return nest.map_structure(lambda t: tf.squeeze(t, 0), outputs), core_state
+
+  @snt.reuse_variables
+  def unroll(self, actions, env_outputs, core_state):
+    _, _, done, _ = env_outputs
+
+    torso_outputs = snt.BatchApply(self._torso)((actions, env_outputs))
+
+    # Note, in this implementation we can't use CuDNN RNN to speed things up due
+    # to the state reset. This can be XLA-compiled (LSTMBlockCell needs to be
+    # changed to implement snt.LSTMCell).
+    initial_core_state = self._core.zero_state(tf.shape(actions)[1], tf.float32)
+    core_output_list = []
+    for input_, d in zip(tf.unstack(torso_outputs), tf.unstack(done)):
+      # If the episode ended, the core state should be reset before the next.
+      core_state = nest.map_structure(functools.partial(tf.where, d),
+                                      initial_core_state, core_state)
+      core_output, core_state = self._core(input_, core_state)
+      core_output_list.append(core_output)
+
+    return snt.BatchApply(self._head)(tf.stack(core_output_list)), core_state
+
+class DMLabImpalaLSTM(snt.RNNCore):
 
   """Agent with ResNet."""
 
   def __init__(self, num_actions):
-    super(ImpalaLSTM, self).__init__(name='impala_lstm')
+    super(DMLabImpalaLSTM, self).__init__(name='dmlab_impala_lstm')
 
     self._num_actions = num_actions
 
@@ -567,7 +662,7 @@ def agent_factory(agent_name):
   specific_agent = {
     'ImpalaFeedForward'.lower(): ImpalaFeedForward,
     'PopArtFeedForward'.lower(): PopArtFeedForward,
-    'ImpalaLSTM'.lower(): ImpalaLSTM,
+    'DMLabImpalaLSTM'.lower(): DMLabImpalaLSTM,
     'PopArtLSTM'.lower(): PopArtLSTM
   }
 
