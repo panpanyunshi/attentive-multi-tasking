0d88ea2886e0e1a6ab071f11bcb475bae7b93027
steffenvan <steffem@vanfamily.com>
2019-07-23 16:53:27 +0200

    Using the deep network

--- agent.py
+++ agent.py
@@ -88,12 +88,29 @@ class ImpalaFeedForward(snt.AbstractModule):
 
     with tf.variable_scope('convnet'):
       conv_out = frame
-      conv_out = snt.Conv2D(16, 8, stride=4)(conv_out)
-      conv_out = tf.nn.relu(conv_out)
-      conv_out = snt.Conv2D(32, 4, stride=2)(conv_out)
+      for i, (num_ch, num_blocks) in enumerate([(16, 2), (32, 2), (32, 2)]):
+        # Downscale.
+        conv_out = snt.Conv2D(num_ch, 3, stride=1, padding='SAME')(conv_out)
+        conv_out = tf.nn.pool(
+            conv_out,
+            window_shape=[3, 3],
+            pooling_type='MAX',
+            padding='SAME',
+            strides=[2, 2])
+
+        # # Residual block(s).
+        for j in range(num_blocks):
+          with tf.variable_scope('residual_%d_%d' % (i, j)):
+            block_input = conv_out
+            conv_out = tf.nn.relu(conv_out)
+            conv_out = snt.Conv2D(num_ch, 3, stride=1, padding='SAME')(conv_out)
+            conv_out = tf.nn.relu(conv_out)
+            conv_out = snt.Conv2D(num_ch, 3, stride=1, padding='SAME')(conv_out)
+            conv_out += block_input
 
     conv_out = tf.nn.relu(conv_out)
     conv_out = snt.BatchFlatten()(conv_out)
+    
     conv_out = snt.Linear(256)(conv_out)
     conv_out = tf.nn.relu(conv_out)
 
